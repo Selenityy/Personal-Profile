17a83cab360f1dcdbf399fc4e458649a8f559c6e
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-09-03 19:18:17 -0400

    Created Work Experience section and entry components for the Resume part of the website

---
+++ src/components/WorkExperienceEntry.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const WorkExperienceEntry = ({
+  title,
+  company,
+  startDate,
+  endDate,
+  responsibilities,
+}) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <h4>{company}</h4>
+      <time dateTime={startDate}>
+        {new Date(startDate).toLocaleDateString("en-US", {
+          month: "2-digit",
+          year: "numeric",
+        })}
+      </time>{" "}
+      -{" "}
+      <time dateTime={endDate}>
+        {new Date(endDate).toLocaleDateString("en-US", {
+          month: "2-digit",
+          year: "numeric",
+        })}
+      </time>
+      <ul>
+        {responsibilities.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default WorkExperienceEntry;

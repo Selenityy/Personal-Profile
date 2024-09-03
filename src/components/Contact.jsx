1ee6bb784e82f79eb7f4ff57e73b911e846f3759
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-09-03 19:30:30 -0400

    Created a template contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section>
+      <div>
+        <div>Form</div>
+        <div>Name</div>
+        <div>Email</div>
+        <div>Message</div>
+        <button>Submit</button>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;

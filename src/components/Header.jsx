fdefe9729d45a26dd5731dc9af9e734c67772f2b
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-09-04 13:50:27 -0400

    Added Dark and Light mode to the header, as well as class names for styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdOutlineLightMode } from "react-icons/md";
 
-const Header = () => {
+const Header = ({ toggleDarkMode, isDarkMode }) => {
   const router = useRouter();
 
   const homeClick = () => {
@@ -27,14 +29,54 @@ const Header = () => {
   };
 
   return (
-    <header className="sticky top-0 z-50">
+    <header className="container mx-auto py-4 max-w-4xl background-floral">
       <nav className="flex justify-between">
-        <div onClick={homeClick}>Home</div>
-        <div onClick={aboutClick}>About</div>
-        <div onClick={projectsClick}>Projects</div>
-        <div onClick={resumeClick}>Resume</div>
-        <div onClick={contactClick}>Contact</div>
+        <div
+          onClick={homeClick}
+          className="font-poppins text-color hover:font-bold active:font-bold focus:font-bold cursor-pointer"
+          tabIndex={0}
+        >
+          Home
+        </div>
+        <div
+          onClick={aboutClick}
+          className="font-poppins text-color hover:font-bold active:font-bold focus:font-bold cursor-pointer"
+          tabIndex={0}
+        >
+          About
+        </div>
+        <div
+          onClick={projectsClick}
+          className="font-poppins text-color hover:font-bold active:font-bold focus:font-bold cursor-pointer"
+          tabIndex={0}
+        >
+          Projects
+        </div>
+        <div
+          onClick={resumeClick}
+          className="font-poppins text-color hover:font-bold active:font-bold focus:font-bold cursor-pointer"
+          tabIndex={0}
+        >
+          Resume
+        </div>
+        <div
+          onClick={contactClick}
+          className="font-poppins text-color hover:font-bold active:font-bold focus:font-bold cursor-pointer"
+          tabIndex={0}
+        >
+          Contact
+        </div>
       </nav>
+      <div className="pr-4">
+        {isDarkMode ? (
+          <MdOutlineDarkMode
+            onClick={toggleDarkMode}
+            className="fill-white size-5"
+          />
+        ) : (
+          <MdOutlineLightMode onClick={toggleDarkMode} className="size-5" />
+        )}
+      </div>
     </header>
   );
 };

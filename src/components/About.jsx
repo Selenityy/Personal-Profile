fe2ba06d296837982570c78cac885207d77368f1
Selena Rodriguez <selena.k.rodriguez@gmail.com>
2024-09-05 13:25:01 -0400

    Added images to the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,22 +16,39 @@ const About = () => {
       <h1 className="font-victorian-demo text-8xl font-bold text-color font-light">
         About
       </h1>
-      <img
-        src="/images/profilePic.png"
-        alt="Profile Picture"
-        className="rounded-full object-cover size-72"
-      />
-      <p className="font-butler">Hi, my name is Selena Rodriguez!</p>
-      <p className="font-butler">
-        I have always been a very curious person and someone who wants to keep
-        learning. With a background in psychology, content creation, gaming, and
-        esports, I have a very unique skill set that blends my creative mind
-        with my analytical one. Being adaptable and a fast learner brought me to
-        teaching myself how to code and create websites! I bring my energy to
-        all my creations and love expressing emotions, aesthetics, or
-        personality to my websites.
-      </p>
-      <div>
+      <div className="flex items-center justify-between">
+        <div className="relative w-80 h-80 my-8">
+          {/* Frame Image */}
+          <img
+            src="/images/victorian-frame2.png"
+            alt="Victorian Frame"
+            className="w-full h-full z-30"
+          />
+
+          {/* Profile Picture */}
+          <img
+            src="/images/profilePic.png"
+            alt="Profile Picture"
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-56 h-56 rounded-full object-cover"
+          />
+        </div>
+        <div className="w-1/2">
+          <div className="flex items-center flex-wrap space-x-1">
+            <p className="font-butler text-lg w-full">Hi, my name is</p>
+            <p className="font-butler text-lg w-full">Selena Rodriguez</p>
+          </div>
+          <p className="font-butler text-lg w-full text-wrap">
+            I have always been a very curious person and someone who wants to
+            keep learning. With a background in psychology, content creation,
+            gaming, and esports, I have a very unique skill set that blends my
+            creative mind with my analytical one. Being adaptable and a fast
+            learner brought me to teaching myself how to code and create
+            websites! I bring my energy to all my creations and love expressing
+            emotions, aesthetics, or personality to my websites.
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-20">
         <IoIosArrowDown
           onClick={onArrowClick}
           className="animate-bounce fill-black size-8"
